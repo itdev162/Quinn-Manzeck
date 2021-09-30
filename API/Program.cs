f2a232816495072e2deb8920010b9b446c2c9ac2
QuinnManzeckk <89595510+QuinnManzeckk@users.noreply.github.com>
2021-09-30 10:22:00 -0500

    gitignore fix

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 namespace API
